3ed67e604a1ee20e7463165e66d1673a9b471d9a
Akshaj Bansal <161148217+0AkshajBansal0@users.noreply.github.com>
2025-01-02 02:37:58 +0530

    hero section done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import { HERO_CONTENT } from "../constants";
+import profilePic from "../assets/AkshajBansalProfile.jpg";
 
 const Hero = () => {
   return (
@@ -12,7 +13,14 @@ const Hero = () => {
             <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
               Full Stack Developer
             </span>
-            <p className="my-2 max-w-xl py-6 font-light tracking-tighter">{HERO_CONTENT}</p>
+            <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
+              {HERO_CONTENT}
+            </p>
+          </div>
+        </div>
+        <div className="w-full lg:w-1/2 lg:p-8">
+          <div className="flex justify-center">
+            <img src={profilePic} alt="Akshaj Bansal" className="w-[240px] rounded-3xl" />
           </div>
         </div>
       </div>
