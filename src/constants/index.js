87f5c010b7690d74c1092a5450a95ba4409c100c
Akshaj Bansal <161148217+0AkshajBansal0@users.noreply.github.com>
2025-01-04 00:46:23 +0530

    detail changes

--- src/constants/index.js
+++ src/constants/index.js
@@ -8,32 +8,24 @@ export const ABOUT_TEXT = `I am a dedicated and versatile Front-End developer wi
 
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+    year: "2023 - 2027",
+    role: "B.Tech in Computer Science Engineering",
+    company: "SRM Institute of Science and Technology, Kattankulathur",
+    description: `I am currently pursuing a B.Tech degree at SRMIST. I achieved a 10.00 GPA in my first semester, followed by a 9.90 GPA in the second semester, resulting in an impressive cumulative CGPA of 9.95.`,
+    technologies: ["Python", "C++", "Java", "HTML5", "CSS3", "JS", "React.js", "FireBase", "mongoDB", "MySQL"],
   },
   {
     year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
-  },
-  {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+    role: "12th Class",
+    company: "V.V. D.A.V. Public School, Delhi",
+    description: `Passed with 85.6% in 12th Board Exams`,
+    technologies: ["Python", "MySQL"],
   },
   {
     year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+    role: "10th Class",
+    company: "V.V. D.A.V. Public School, Delhi",
+    description: `Passed with 96.2% in 10th CBSE Exams`,
   },
 ];
 
