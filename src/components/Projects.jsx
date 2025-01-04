97ee532d9960a240e997db2494a190bd161238ad
Akshaj Bansal <161148217+0AkshajBansal0@users.noreply.github.com>
2025-01-05 01:19:26 +0530

    projects section done

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,26 @@
+import { PROJECTS } from "../constants"
+
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div className="border-b border-neutral-900 pb-4">
+      <h2 className="my-20 text-center text-4xl">Projects</h2>
+      <div>
+        {PROJECTS.map((project, index) => (
+          <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
+            <div className="w-full lg:w-1/4 lg:mb-8">
+              <img src={project.image} alt={project.title} className="rounded w-full md:w-[600px] lg:w-[800px]" />
+            </div>
+            <div className="w-full max-w-xl lg:w-3/4 lg:pl-8">
+              <h6 className="mb-2 font-semibold py-1">{project.title}</h6>
+              <p className="mb-2 text-neutral-400">{project.description}</p>
+              {project.technologies.map((tech, index) => (
+                <span key={index} className="mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-900">{tech}</span>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
