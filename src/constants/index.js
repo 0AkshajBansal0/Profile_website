77f21200dd77521a8110a3211d2d65e39e07316c
Akshaj Bansal <161148217+0AkshajBansal0@users.noreply.github.com>
2025-01-07 21:28:37 +0530

    framer motion

--- src/constants/index.js
+++ src/constants/index.js
@@ -53,9 +53,3 @@ export const PROJECTS = [
     technologies: ["HTML", "Tailwind-CSS", "React.js",],
   },
 ];
-
-export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
-};
